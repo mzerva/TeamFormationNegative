35ac3652f0e191a347bcfd7f3299e80107c49f09
mzerva <mzerva@cs.uoi.gr>
2017-06-12 17:22:36 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,13 @@ public class Main {
 		String pairValuesPath="data\\?";
 		String networkPath="data\\epinions\\network.txt"; 
 		String userPath="data\\epinions\\users.txt";
-		String skillPath="data\\epinions\\skill_distribution.txt";
+		String skillPath="data\\epinions\\skills.txt";
 		
 		//SlashDot
 		/*String pairValuesPath="data\\?";
 		String networkPath="data\\slashdot\\network.txt"; 
 		String userPath="data\\slashdot\\users.txt";
-		String skillPath="data\\slashdot\\skill_distribution.txt";*/
+		String skillPath="data\\slashdot\\skills.txt";*/
 				
 		InputManager manager = new InputManager(pairValuesPath,networkPath,userPath,skillPath);
 		TeamFormationAlgorithm algorithm = new TeamFormationAlgorithm(manager.getNetwork(), manager.getSkillInfo(),manager.getPairInfo());
