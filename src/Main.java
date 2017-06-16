f5dbb2fa47d64f4be5a743a1b10e6891c827a298
mzerva <mzerva@cs.uoi.gr>
2017-06-16 18:24:55 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,24 @@ public class Main {
 	public static void main(String[] args){
 		
 		//Epinions
-		String pairValuesPath="data\\?";
+		/*
+		//String pairValuesPath="data\\epinions\\more_positive_paths_compatibility.txt";
+		//String pairValuesPath="data\\epinions\\one_positive_path_compatibility.txt";
+		String pairValuesPath="data\\epinions\\no_negative_paths_compatibility.txt";
 		String networkPath="data\\epinions\\network.txt"; 
 		String userPath="data\\epinions\\users.txt";
-		String skillPath="data\\epinions\\skills.txt";
+		String skillPath="data\\epinions\\skills.txt";*/
 		
 		//SlashDot
-		/*String pairValuesPath="data\\?";
+		//String pairValuesPath="data\\slashdot\\more_positive_paths_compatibility.txt";
+		//String pairValuesPath="data\\slashdot\\one_positive_path_compatibility.txt";
+		String pairValuesPath="data\\slashdot\\no_negative_paths_compatibility.txt";
 		String networkPath="data\\slashdot\\network.txt"; 
 		String userPath="data\\slashdot\\users.txt";
-		String skillPath="data\\slashdot\\skills.txt";*/
+		String skillPath="data\\slashdot\\skills.txt";
 				
 		InputManager manager = new InputManager(pairValuesPath,networkPath,userPath,skillPath);
-		TeamFormationAlgorithm algorithm = new TeamFormationAlgorithm(manager.getNetwork(), manager.getSkillInfo(),manager.getPairInfo());
+		TeamFormationAlgorithm algorithm = new TeamFormationAlgorithm(manager.getNetwork(), manager.getSkillInfo(),manager.getPairInfo(),manager.getCompatibleDistances());
 		algorithm.start();
 	}
 }
