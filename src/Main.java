b2d08159633a33d5b3a5c286c7c958760d9dd0eb
mzerva <mzerva@cs.uoi.gr>
2017-06-17 15:15:11 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,17 @@ public class Main {
 		
 		//Epinions
 		/*
-		//String pairValuesPath="data\\epinions\\more_positive_paths_compatibility.txt";
-		//String pairValuesPath="data\\epinions\\one_positive_path_compatibility.txt";
-		String pairValuesPath="data\\epinions\\no_negative_paths_compatibility.txt";
+		//String pairValuesPath="data\\epinions\\more_positive_paths.txt";
+		//String pairValuesPath="data\\epinions\\one_positive_path.txt";
+		String pairValuesPath="data\\epinions\\no_negative_paths.txt";
 		String networkPath="data\\epinions\\network.txt"; 
 		String userPath="data\\epinions\\users.txt";
 		String skillPath="data\\epinions\\skills.txt";*/
 		
 		//SlashDot
-		//String pairValuesPath="data\\slashdot\\more_positive_paths_compatibility.txt";
-		//String pairValuesPath="data\\slashdot\\one_positive_path_compatibility.txt";
-		String pairValuesPath="data\\slashdot\\no_negative_paths_compatibility.txt";
+		//String pairValuesPath="data\\slashdot\\more_positive_paths.txt";
+		//String pairValuesPath="data\\slashdot\\one_positive_path.txt";
+		String pairValuesPath="data\\slashdot\\no_negative_paths.txt";
 		String networkPath="data\\slashdot\\network.txt"; 
 		String userPath="data\\slashdot\\users.txt";
 		String skillPath="data\\slashdot\\skills.txt";
