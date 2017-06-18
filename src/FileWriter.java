88a31d8900b9683b8201bd066f87ae9650e8197f
mzerva <mzerva@cs.uoi.gr>
2017-06-19 00:09:51 +0300

    Add files via upload

---
+++ src/FileWriter.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+public class FileWriter {
+	private String outputPath;
+	private PrintWriter outputWriter;
+	private File file;
+	
+	public FileWriter(String outputPath){
+		this.outputPath=outputPath;
+	}
+
+	public void initWriter(){
+		file = new File(outputPath);
+		try 
+		 { 
+			outputWriter = new PrintWriter(new FileOutputStream(outputPath)); 
+		 } 
+		 catch(FileNotFoundException e) 
+		 { 
+			 System.out.printf("Error opening the file %s.\n",outputPath);
+		 }
+	}
+	
+	public void writeData(ArrayList<String> data){
+		for(int i=0;i<data.size();i++){
+			outputWriter.println(data.get(i));
+		}
+		outputWriter.close();
+	}
+	
+}
